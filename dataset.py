679fd35bc29da9d10714249d48d4b896e23ffe70
alexanderdavila <adolfodiaz@unicauca.edu.co>
2021-12-06 16:51:47 -0500

    creacion dataset archivo v1

---
+++ dataset.py
@@ -0,0 +1,22 @@
+import tensorflow as tf
+
+
+def main():
+
+
+
+    print(tf.__version__)
+    # dataset = tf.data.Dataset.from_tensor_slices([1, 2, 3])
+    # for element in dataset:
+    #     print(element)
+    #     tf.Tensor(1, shape=(), dtype=int32)
+    #     tf.Tensor(2, shape=(), dtype=int32)
+    #     tf.Tensor(3, shape=(), dtype=int32)
+
+
+
+
+
+
+if __name__=="__main__":
+    main()
