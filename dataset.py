3cc404054c5bf983b1e4ee92f6bbb2d46a94822e
alexanderdavila <adolfodiaz@unicauca.edu.co>
2021-12-06 23:37:28 -0500

    lectura input numeros

--- dataset.py
+++ dataset.py
@@ -6,12 +6,12 @@ def main():
 
 
     print(tf.__version__)
-    # dataset = tf.data.Dataset.from_tensor_slices([1, 2, 3])
-    # for element in dataset:
-    #     print(element)
-    #     tf.Tensor(1, shape=(), dtype=int32)
-    #     tf.Tensor(2, shape=(), dtype=int32)
-    #     tf.Tensor(3, shape=(), dtype=int32)
+    dataset = tf.data.Dataset.from_tensor_slices([1, 2, 3])
+    for element in dataset:
+        print(element)
+        tf.Tensor(1, shape=(), dtype=int32)
+        tf.Tensor(2, shape=(), dtype=int32)
+        tf.Tensor(3, shape=(), dtype=int32)
 
 
 
